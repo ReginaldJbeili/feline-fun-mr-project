8407045c7424d4d175be09173359e07ebd7f68e3
Maria <m.olanipekun@hotmail.com>
2020-01-16 15:18:37 +0000

    Connect cat info and dropdown with app.js

--- src/components/CatDropdown.js
+++ src/components/CatDropdown.js
@@ -1,30 +1,17 @@
 import React from 'react'
-import getAllCatBreeds from '../utils/getAllCatBreeds';
+// import getAllCatBreeds from '../utils/getAllCatBreeds';
 
-const CatDropdown = () => {
-    const [catData, setCatData] = React.useState(null);
+const CatDropdown = props => {
 
-    React.useEffect(() => {
-        getAllCatBreeds().then(data => setCatData(data));
-    }, []);
 
-    console.log('This is all the cat data', catData);
+    if(!props.catData) {return <h2>Loading...</h2>}
 
-    if (!catData) {return <h2>Loading...</h2>}
-
-    // // use chosenCat.value to find the selected cat
-    // let chosenCat = document.getElementById("chosenCat");
-    // let chooseCatBtn = document.getElementById("chosenCatBtn");
-
-    // chooseCatBtn.addEventListener('click', ()=> {
-        
-    //     console.log("This is the cat I choose!", chosenCat)
-    // })
     return (
         <div>
-            <select id="chosenCat">
-                    {catData.map( cats => {
-                        return (<option key={cats.id}> {cats.name} </option>)
+            <select value={props.selectedCat} onChange = {event => {  props.setSelectedCat(event.target.value) }}>
+                    {props.catData.map( cats => {
+                        
+                        return (<option key={cats.id} value={cats.id}> {cats.name} </option>)
                     }
 
                     )}
--- src/components/CatInfo.js
+++ src/components/CatInfo.js
@@ -1,27 +1,21 @@
 import React from 'react'
-import getCatInfo from '../utils/getCatInfo';
 
-const CatInfo = () => {
-    const [catInfo, setCatInfo] = React.useState(null);
+const CatInfo = props => {
 
-    React.useEffect(() => {
-        const breedId = 'drex';
-        getCatInfo(breedId).then(data => setCatInfo(data))
-    }, [])
-    console.log(catInfo);
+    
 
-    if(!catInfo) return <h2>cat info loading...</h2>
+    if(!props.catInfo) return <h2>cat info loading...</h2>
 
-    {const catEnergy = `energy ${catInfo[0].breeds[0].energy_level}`
-    const socialNeeds = `social needs ${catInfo[0].breeds[0].social_needs}`
-    const healthIssues = `health issues ${catInfo[0].breeds[0].health_issues}`
+    {const catEnergy = `energy ${props.catInfo[0].breeds[0].energy_level}`
+    const socialNeeds = `social needs ${props.catInfo[0].breeds[0].social_needs}`
+    const healthIssues = `health issues ${props.catInfo[0].breeds[0].health_issues}`
     console.log("this is all info we need", [catEnergy,socialNeeds,healthIssues]);
     }
     return (
         <div>
-            <h1>{catInfo[0].breeds[0].name}</h1>
-            <p>{catInfo[0].breeds[0].description}</p>
-            <img src={catInfo[0].url} alt={`${catInfo[0].breeds[0].name} cat`}></img>
+            <h1>{props.catInfo[0].breeds[0].name}</h1>
+            <p>{props.catInfo[0].breeds[0].description}</p>
+            <img src={props.catInfo[0].url} alt={`${props.catInfo[0].breeds[0].name} cat`}></img>
         </div>
     )
 }
